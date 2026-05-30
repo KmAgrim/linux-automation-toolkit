41a50c362049897e42798e9be19b6234fc1e109f
Kumar Agrim <267532362+KmAgrim@users.noreply.github.com>
2026-05-30 16:50:20 +0530

    Made the System Info Displaying Script More dynamic, somewhat improved formatting.

--- scripts/system_info.sh
+++ scripts/system_info.sh
@@ -1,29 +1,67 @@
 #!/bin/bash
+menu(){
+cat <<- _EOF_
+        ===== SYSTEM INFORMATION =====
+        Press 0 to exit.
+        Press 1 to show Current Username and Hostname.
+        Press 2 to show System Uptime.
+        Press 3 to show System Date.
+        Press 4 to show Disk Usage.
+        Press 5 to show Memory Usage.
+        Press 6 to show Ip Address.
+_EOF_
+}
 
-echo "===== SYSTEM INFORMATION ====="
+support(){
+cat <<- _EOF_
+Correct Options are 0 1 2 3 4 5 6
+Only A single digit for example -> 0 Not 01
+_EOF_
+}
 
-echo ""
 
-echo "Hostname: $(hostname)"
 
-echo "Current User: $(whoami)"
-echo 
+while true; do
+clear
+menu
+read -p "Pls Enter Your Choice > "
+if [[ -z "$REPLY" ]]; then
+echo "No Option given. Pls Provide an Option." >&2
+sleep 2
+continue
+elif [[ ! "$REPLY" =~ ^[[:digit:]]+$ ]]; then
+echo "Invalid format given." >&2
+support
+sleep 3
+continue
+elif [[ ! "$REPLY" =~ ^[0123456]{1}$ ]]; then
+echo "Given Option is Not Valid." >&2
+support
+sleep 5
+continue
+else
+if ((REPLY == 0)); then
+echo "Exiting."
+break
+elif ((REPLY == 1)); then
+echo -e "\nHostname: $(hostname)\tCurrent User: ${USER}\n"
+elif ((REPLY == 2)); then
+echo -e "Uptime: $(uptime)\n"
+elif ((REPLY == 3)); then
+echo -e "Current System Date:$(date)\n"
+elif ((REPLY == 4)); then
+echo -e "====\tCurrent Disk Usage\t===="
+echo -e "\n$(df -h / --output=size,used,avail,pcent)\n"
 
-echo "Uptime: $(uptime)"
-echo
-
-echo "Current System Date:"
-date
-echo 
-
-echo "==== Curren Disk Usage ===="
-df -h /
-echo
-
+elif ((REPLY == 5)); then
 echo "==== Current Memory Usage ===="
-free -h
-echo
+echo -e "$(free -h | tr -s '[:blank:]' '\t' | cut -f 1-4,7)\n"
 
-
-echo "IP Address:"
-ip -4 a | grep -w "inet" | grep -w "ens33"
+elif ((REPLY == 6)); then
+echo -e "IP Address  $(ip -br a)"
+fi
+if [[ "$REPLY" == [1-5] ]]; then
+sleep 5
+fi
+fi
+done
