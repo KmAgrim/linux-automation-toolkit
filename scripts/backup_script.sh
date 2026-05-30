b6138ffbee34a6a08324ab3decfc20194ccab84e
Kumar Agrim <267532362+KmAgrim@users.noreply.github.com>
2026-05-30 11:53:15 +0530

    Minor Improvement to backup script.

--- scripts/backup_script.sh
+++ scripts/backup_script.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
-usage_msg="Usage: $0 source_dir backup_dir"
+USAGE() {
+echo "Usage: $0 source_directory backup_directory."
+}
+
 if [ "$#" -ne 2 ]; then
-echo "$usage_msg"
-exit
+echo "Number of argument greater or less than 2!. Exiting." >&2
+USAGE
+exit 1
 else
-if [ -d "$1" ] && [ -d "$2" ]; then
+if [[ -d "$1"  && -d "$2" ]]; then # validating whether if user given path are correct or not.
 timestamp=$(date +%Y-%m-%d-%H-%M)
 tarname="backup_$timestamp.tar.gz"
-tar -czf "$2/$tarname" "$1" &> /dev/null
-if [ "$?" -eq 0 ]; then
+if tar -czf "$2/${tarname}" "$1"; then
 echo "Backup created: in directory $2 named $tarname"
 else
-echo  "Backup Not Created. Exiting"
+echo  "Backup Not Created. Exiting" >&2
+exit 1
 fi
-elif [ ! -d "$1"  ]; then
-echo "Source directory path is  incorrect. $usage_msg"
-exit
 else
-echo "Destination directory path is incorrect. $usage_msg"
-exit
+echo "Source or Destination directory path is incorrect." >&2
+USAGE
+exit 1
 fi
 fi
 
