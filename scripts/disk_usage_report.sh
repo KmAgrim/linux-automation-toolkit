ca79e967bed0ac1828ef9d6cfca678bb7d72c84a
Kumar Agrim <267532362+KmAgrim@users.noreply.github.com>
2026-06-07 17:23:30 +0530

    Working on Disk usage report utility.

---
+++ scripts/disk_usage_report.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+USAGE(){
+    local err="$1"
+    if (( err == 1 )); then
+    cat <<- _EOF_
+Usage: ./${0##*/} PATH
+Example 
+./${0##*/} logs/
+Exiting!
+_EOF_
+fi
+}
+valid_ArgC(){
+    local args="$1"
+    if (( args == 0)); then
+    echo "No Argument Provided." >&2
+    USAGE 1
+    return 1
+    elif (( args > 1)); then
+    echo "More than one Argument Provided." >&2
+    USAGE 1
+    return 1
+    else
+    return 0
+    fi
+}
+valid_Dir(){
+    local dir="$1"
+    if [[ ! -e "$dir" ]]; then
+    echo -e "Given Path does not exit!\n" >&2
+    USAGE 1
+    return 1
+    elif [[ ! -d "$dir" ]]; then
+    echo -e "Given Path is not a directory!\n" >&2
+    USAGE 1
+    return 1
+    else
+    return 0
+    fi
+}
+TotalSize(){
+    local dir="$1"
+    local TSize=0
+    TSize="$(cut -f 1 < <(du -sh "$dir"))"
+cat <<- _EOF_
+Total Size:
+$TSize
+_EOF_
+    return 0
+}
+LargestDir(){
+    local dir="$1"
+cat <<- _EOF_
+Largest Directories
+$(du -h -S --max-depth=1 "$dir" | sort -hr)
+_EOF_
+    return 0
+}
+
+LargestFile(){
+    local dir="$1"
+cat <<- _EOF_
+Largest Files
+$(du --all -S -h "$dir" | sort -hr | head )
+_EOF_
+    return 0
+}
+
+
+if valid_ArgC "$#"; then
+if valid_Dir "$@"; then
+TotalSize "$@"
+LargestDir "$@"
+LargestFile "$@"
+fi
+fi
